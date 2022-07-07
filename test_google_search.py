8ac5d845ababf980dee09a3823dd43641d2a98b6
Daria Spiridonova <daria.spiridonova@myget-it.com>
2022-07-07 14:19:56 +0300

    Update

--- test_google_search.py
+++ test_google_search.py
@@ -2,12 +2,14 @@ from selene import have, be
 from selene.support.shared import browser
 
 
+# Positive test
 def test_right_search(open_google):
     query = browser.element('[name=q]')
     query.should(be.blank).type('Selene').press_enter()
     browser.element('#search').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
+# Negative test
 def test_wrong_search(open_google):
     query = browser.element('[name=q]')
     query.should(be.blank).type('Wrongtext').press_enter()
